e42c84fab53797a34502fe248839b7b86555e8fc
LVotto <luizvotto@gmail.com>
2014-12-06 11:15:44 -0200

    Create Item.hh

---
+++ Item.hh
@@ -0,0 +1,46 @@
+#ifndef ITEM_H_
+#define ITEM_H_
+
+#include <string>
+#include "Character.hh"
+using namespace std;
+
+class Character;
+
+class Item{
+
+protected:
+    string name;
+    double price;
+    Character *owner;
+
+public:
+    Item();
+
+    Item(string name, double price);
+
+    Item(Item* item);
+
+    string getName();
+
+    double getPrice();
+
+    void setOwner(Character*);
+
+    Character* getOwner();
+
+    virtual void use() = 0;
+
+    virtual int getDefensePts() = 0;
+
+    virtual int getAttackPts() = 0;
+
+    virtual bool isEquipped();
+
+    virtual void equip() = 0;
+
+    virtual ~Item();
+
+};
+
+#endif // ITEM_H_
